4dbf872fe184ee9f7dbbfdc542602498fe65aa6c
Yuchan Lee <lasuillard@gmail.com>
2025-04-02 16:08:05 +0000

    refactor: Better auto reloading

--- scripts/run-nginx-autoreloader.sh
+++ scripts/run-nginx-autoreloader.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# Wait for the NGINX service to start
+sleep 5
+
 while true; do
-	inotifywatch --event modify,create,delete --timeout 5 /app/mockbook/nginx/ |
-		kill -s HUP "$(cat /var/run/nginx.pid)"
+	inotifywait --quiet --event modify,create,delete --recursive /app/mockbook/nginx/ |
+		while read -r CHANGE; do
+			kill -s HUP "$(cat /var/run/nginx.pid)"
+			echo "Reloaded NGINX configuration due to change: $CHANGE"
+		done
 done
